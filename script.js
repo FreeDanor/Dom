236d9804e80162d726d5156bc96b011727a0a605
FreeDanor <ortizmendez_d@hotmail.com>
2022-11-02 21:32:43 -0500

    clase03 completa

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const btn = document.querySelector("[data-form-btn]");
 const input = document.querySelector("[data-form-input]");
+const list = document.querySelector("[data-list]")
+
 
 //Arrow funtions o funciones anonimas
 const createTask = (evento) => {
     evento.preventDefault();
-    console.log(input.value);
+    const value = input.value;
+    const task = document.createElement("li");
+    task.classList.add("card")
+    input.value = "";
+    //Template strings o plantilla
+    const content = `<div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.innerHTML = content;
+
+    list.appendChild(task);
+
+    console.log(content);
 }
 
 console.log(btn);
