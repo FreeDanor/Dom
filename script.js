a40a41d2483d7ce32865430c56f0ce2c11cb04c0
FreeDanor <ortizmendez_d@hotmail.com>
2022-11-02 23:48:09 -0500

    clase04 completada

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
+// Immediately invoked function expression IIFE
+( () => {
+
 const btn = document.querySelector("[data-form-btn]");
 const input = document.querySelector("[data-form-input]");
 const list = document.querySelector("[data-list]")
 
-
 //Arrow funtions o funciones anonimas
-const createTask = (evento) => {
-    evento.preventDefault();
+const createTask = (event) => {
+    event.preventDefault();
     const value = input.value;
     const task = document.createElement("li");
     task.classList.add("card")
     input.value = "";
     //Template strings o plantilla
-    const content = `<div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML = content;
-
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titletask = document.createElement("span");
+    titletask.classList.add("task");
+    titletask.innerText = value
+    taskContent.appendChild(titletask);
+    const content = `
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    
+    //task.innerHTML = content;
+    task.appendChild(taskContent);
     list.appendChild(task);
-
-    console.log(content);
 }
 
-console.log(btn);
-
 btn.addEventListener("click", createTask);
+
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click",completeTask)
+    return i;
+}
+
+const completeTask = (event) => {
+    const element = event.target
+    //Target para seleccionar el elemento
+    element.classList.toggle("fas");
+    //la tag toggle revisa la clase (quita o pone)
+    element.classList.toggle("completeIcon");
+    //la clase completeIcon solo esta en el CSS
+    element.classList.toggle("far");
+}
+
+})()
